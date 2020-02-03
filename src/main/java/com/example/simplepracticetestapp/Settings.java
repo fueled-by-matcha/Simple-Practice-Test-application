75a764e2446cb5589426881a5203576ad2035ee6
Lauren <lauren.lizzie.johnson@gmail.com>
2020-02-03 13:39:05 -0500

    Update Settings
    Aligned text fields, updated Settings.java

--- src/main/java/com/example/simplepracticetestapp/Settings.java
+++ src/main/java/com/example/simplepracticetestapp/Settings.java
@@ -7,12 +7,22 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class Settings extends AppCompatActivity {
+    private int questionNum;
+    private int passGrade;
+    private SeekBar setNum;
+    private EditText passingGrade;
+    private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        start = (Button) findViewById(R.id.startBtn);
+        passingGrade = (EditText) findViewById(R.id.setGrade);
+        setNum = (SeekBar) findViewById(R.id.setNumberQuestions);
     }
 
 }

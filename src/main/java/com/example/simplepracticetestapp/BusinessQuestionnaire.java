a0dc8ef30c6ccdea584f2ce2d24b0150e15ec7b6
Lauren <lauren.lizzie.johnson@gmail.com>
2020-02-03 21:50:51 -0500

    Update BusinessQuestionnaire.java

--- src/main/java/com/example/simplepracticetestapp/BusinessQuestionnaire.java
+++ src/main/java/com/example/simplepracticetestapp/BusinessQuestionnaire.java
@@ -3,14 +3,19 @@ package com.example.simplepracticetestapp;
 public class BusinessQuestionnaire {
     public String questions[]={
         "What is an organization?",
+            "What is the best definition of Efficiency?",
+            "What is the best definition of Effectiveness?"
     };
 
     public String choices[][]={
         {"A deliberate arrangement of people who act together to accomplish some specific purpose", "A group of people", "Individuals", "N/A"},
+            {"Most output from least input", "Completing activities so that goals are achieved", "Least output with most input", "Not meeting goals"},
     };
 
     public String answers[]={
         "A deliberate arrangement of people who act together to accomplish some specific purpose",
+            "Most output from least input",
+            
     };
     public String getQuestion(int a){
         String question = questions[a];
